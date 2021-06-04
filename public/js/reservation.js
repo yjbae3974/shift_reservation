e54a77977fd7d4e5f7db6dd6ff1eb3bc30a4a381
yjbae3974 <brianbae6@korea.ac.kr>
2021-06-04 21:57:27 +0900

    .

--- public/js/reservation.js
+++ public/js/reservation.js
@@ -14,6 +14,23 @@ function animateValue(obj, start, end, duration) {
   const obj_1 = document.getElementById("value");
   const obj_2 = document.getElementById("value_2");
   const obj_3 = document.getElementById("value_3");
-  animateValue(obj_1, 0, 12384, 2000);
+  let statPosition = document.getElementById("statistics").getBoundingClientRect();
+  console.log(statPosition)
+  
+    animateValue(obj_1, 0, 12384, 2000);
   animateValue(obj_2, 0, 1795, 2000);
   animateValue(obj_3, 0, 1053, 2000);
+  
+  function getOffset( el ) {
+    var _x = 0;
+    var _y = 0;
+    while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
+        _x += el.offsetLeft - el.scrollLeft;
+        _y += el.offsetTop - el.scrollTop;
+        el = el.offsetParent;
+    }
+    return { top: _y, left: _x };
+}
+var x = getOffset( document.getElementById('statistics') ).left; 
+console.log(x)
+  
