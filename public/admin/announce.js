3a1b48f3377d3a0ec55e6b4c3db1506d05186d4a
yjbae3974 <brianbae6@korea.ac.kr>
2021-06-22 00:38:30 +0900

    .

---
+++ public/admin/announce.js
@@ -0,0 +1,15 @@
+// Initialize and add the map
+function initMap() {
+    // The location of your company 37.580569, 127.028574
+    const location = { lat: 37.580569, lng: 127.028574 };
+    // The map, centered at selected location
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 17,
+      center: location,
+    });
+    // The marker, positioned at location
+    const marker = new google.maps.Marker({
+      position: location,
+      map: map,
+    });
+  }
