f291b9991e67a1d41e661ce7e783069b208727b8
jstep750 <jstep0000@gmail.com>
2021-07-03 04:02:52 +0900

    admin 글쓰기/편집 완성

--- public/admin/admin2.js
+++ public/admin/admin2.js
@@ -1,30 +1,3 @@
-// Initialize and add the map
-function initMap() {
-    // The location of your company 37.580569, 127.028574
-    const location = { lat: 37.580569, lng: 127.028574 };
-    // The map, centered at selected location
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 17,
-      center: location,
-    });
-    // The marker, positioned at location
-    const marker = new google.maps.Marker({
-      position: location,
-      map: map,
-    });
-  }
-  function changeLocation(text){
-    location.href = text;
-   }
-$(document).ready(function(){
-  $('#toSave').click(function(){
-    location.href = 'autosave.html';
-  });
-  $('#toEdit').click(function(){
-    location.href = 'edit.html';
-  });
-});
-
 function getAnnounceSnapshot(){
     let ref, snapshot; 
     // 데이터 불러오기
@@ -86,13 +59,6 @@ function getAdminData(Snapshots, check, num){
       console.log(dataArray);
     });
 }
-
-
-var source   = $("#announce-template").html();
-var adminTemplate = Handlebars.compile(source);
-var source2   = $("#number-template").html();
-var numberTemplate = Handlebars.compile(source2);
-const contentNum = 2;
 
 function makeAdminTable(dataArray, num){
     console.log("admintable",dataArray);
@@ -171,4 +137,3 @@ function deleteDoc(title){
 function back(){
   window.history.back();
 }
-
