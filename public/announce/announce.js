00e3312706f322983a6cebb0bd56d3d2fa996bd1
yjbae3974 <brianbae6@korea.ac.kr>
2021-07-08 16:50:24 +0900

    모바일화 진행중

--- public/announce/announce.js
+++ public/announce/announce.js
@@ -16,4 +16,141 @@ function initMap() {
  function changeLocation(text){
   location.href = text;
  }
-  
+const mobileWidth = '(max-width: 768px)';
+const mediaQuery = window.matchMedia(mobileWidth);
+const divtable = document.getElementById('tablediv');
+  if(mediaQuery.matches){
+    console.log('now the width is smaller than 768');
+    document.getElementById('pagination').style.padding = '0px 24px';
+    document.getElementById('mainContainer').style.marginTop = '28px';
+    divtable.innerHTML = `
+    <div style="padding: 0px 24px;">
+                <div class="list-title">
+                  [업데이트]
+                </div>
+                <div class="list-content hr-link">
+                  7.21업데이트 현황, 새로운 캐릭터 추가, 각종 가챠아이템 구비 완료 들어와라 소비자들아
+                </div>
+                <hr>
+              </div>
+              <div style="padding: 0px 24px;">
+                <div class="list-title">
+                  [업데이트]
+                </div>
+                <div class="list-content hr-link">
+                  7.21업데이트 현황, 새로운 캐릭터 추가, 각종 가챠아이템 구비 완료 들어와라 소비자들아
+                </div>
+                <hr>
+              </div>
+              <div style="padding: 0px 24px;">
+                <div class="list-title">
+                  [업데이트]
+                </div>
+                <div class="list-content hr-link">
+                  7.21업데이트 현황, 새로운 캐릭터 추가, 각종 가챠아이템 구비 완료 들어와라 소비자들아
+                </div>
+                <hr>
+              </div>
+              <div style="padding: 0px 24px;">
+                <div class="list-title">
+                  [업데이트]
+                </div>
+                <div class="list-content hr-link">
+                  7.21업데이트 현황, 새로운 캐릭터 추가, 각종 가챠아이템 구비 완료 들어와라 소비자들아
+                </div>
+                <hr>
+              </div>
+    `
+  }
+  else{
+    console.log('now the width is larger than 768');
+    document.getElementById('mainContainer').style.marginTop = '100px';
+    divtable.innerHTML=`
+    <div class="table-responsive">
+                <!--Table-->
+                <table class="table table-hover">
+        
+                  <!--Table head-->
+                  <thead class="bg-gray">
+                    <tr>
+                      <th>분류</th>
+                      <th>제목</th>
+                      <th>날짜</th>
+                      
+                    </tr>
+                  </thead>
+                  <!--Table head-->
+        
+                  <!--Table body-->
+                  <tbody>
+                    <tr>
+                      <th scope="row">[업데이트]</th>
+                      <td class="hr-link">7.21 업데이트 현황. 새로운 캐릭터 추가. 각종 수정 및 버그 개선</td>
+                      <td>2021.05.19 </td>
+                      
+                    </tr>
+                    <tr>
+                      <th scope="row">[업데이트]</th>
+                      <td>휴대폰 알뜰폰결제 SK7mobile(SK텔링크) 결제 서비스 추가 안내</td>
+                      <td>2021.03.16</td>
+                      
+                    </tr>
+                    <tr>
+                      <th scope="row">[공지]</th>
+                      <td>고객 간담회를 준비하고 있습니다. </td>
+                      <td>2021.03.11</td>
+                      
+                    </tr>
+                    <tr>
+                      <th scope="row">[공지]</th>
+                      <td>적절하지 못한 용어 사용으로 불쾌감을 드려 죄송합니다.</td>
+                      <td>2021.03.09</td>
+                      
+                    </tr>
+                    <tr>
+                      <th scope="row">[공지]</th>
+                      <td>환골탈태의 각오로 고객님들의 신뢰회복에 전심전력을 다하겠습니다.</td>
+                      <td>2021.03.05</td>
+                      
+                    </tr>
+                    <tr>
+                      <th scope="row">[개발현황]</th>
+                      <td>큐브의 잠재능력 재설정 로직과 세부 확률 공개</td>
+                      <td>2021.03.05</td>
+                      
+                    </tr>
+                    <tr>
+                      <th scope="row">[버그수정]</th>
+                      <td>아이템에 부여될 수 있는 모든 종류의 추가옵션이 동일한 확률로 부여되도록 수정됩니다. </td>
+                      <td>2021.02.18</td>
+                      
+                    </tr>
+                    <tr>
+                      <th scope="row">[업데이트]</th>
+                      <td>ver 1.3.344 클라이언트 패치(3:00 ~ 10:00)</td>
+                      <td>2021.02.13</td>
+                      
+                    </tr>
+                    
+                    
+                    <tr>
+                      <th scope="row">[이벤트]</th>
+                      <td>헤파이스토스에 도전하라!</td>
+                      <td>2021.01.01</td>
+                      
+                    </tr>
+                    <tr>
+                      <th scope="row">[업데이트]</th>
+                      <td>패스파인더의 최종데미지 증가 버프 삭제</td>
+                      <td>2020.12.01</td>
+                      
+                    </tr>
+                  </tbody>
+                  <!--Table body-->
+        
+        
+                </table>
+                <!--Table-->
+              </div>
+    `
+  }
--- public/login/login.js
+++ public/login/login.js
@@ -1,5 +1,5 @@
 let change = document.getElementById('whereContentChanges');
-
+let mobile = document.getElementById('mobileChanges');
 function changeContent(content){
     if(content === 'main'){
         change.innerHTML=`
@@ -36,6 +36,39 @@ function changeContent(content){
                       <div class="pointer" onclick="changeContent('password')">Forgot Password?</div>
                     </div>
                   </div>
+        `;
+        mobile.innerHTML=`
+        <div class="sign-up-title text-center" style="color: #fff;padding-top: 50px;">Sign in</div>
+      <div>
+        <input type="email" class="form-control form-rounded mct-5 form-mobile" placeholder="Your email" style="padding: 10px 20px;">
+        <input type="password" class="form-control form-rounded mct-2 form-mobile" placeholder="Your Password" style="padding: 10px 20px;">
+        <div class="d-flex mct-3" style="margin-bottom: 17.6px;">
+            <label class="checkbox-container mx-3 text-start">아이디 저장
+                <input type="checkbox" checked="checked">
+                <span class="checkmark"></span>
+              </label>
+              <label class="checkbox-container text-start">자동로그인
+                <input type="checkbox" checked="checked">
+                <span class="checkmark"></span>
+              </label>
+        </div>
+       
+        <button type="submit" class="btn-rounded text-white text-center" style="
+        font-size: 18px;width: 100%;background-color: #17375E;padding: 12px;">Sign in</button>
+        <div class="separator mt-3" style="color: #fff;">or</div>
+        <button type="submit" class="btn-rounded text-white text-center mt-3" style="
+        font-size: 18px;width: 100%;background-color: #1da1f2;padding: 12px;">Login via <span style="font-weight: 700;">Google</span></button>
+        <button id="register" type="submit" class="btn-rounded text-white text-center mct-1" style="
+        font-size: 18px;width: 100%;background-color: #fae100;padding: 12px;">Login via <span style="font-weight: 700;">Kakao</span></button>
+        <div class="d-flex mt-3 justify-content-center" style="font-size: 18px;">
+          <div style="color: #bfbfbf;">Don't have an Account?</div>
+          <a class="ms-2" style="color: #59dbe0;" href="../register/register.html">Sign up</a>
+        </div>
+        <div class="text-center mt-1" style="font-size: 14px;color: #bd064b;font-weight: 700;">
+          
+          <div class="pointer pb-5" onclick="changeContent('password')">Forgot Password?</div>
+        </div>
+      </div>
         `
     }
     else if(content === 'password'){
@@ -66,6 +99,28 @@ function changeContent(content){
                       
                     </div>
                   </div>
+        `;
+        mobile.innerHTML=`
+        <div class="sign-up-title text-center" style="color: #fff;padding-top: 50px;">Search PW</div>
+      <div>
+        <div style="position: relative;">
+          <input type="email" class="form-control form-rounded mct-5 form-mobile" placeholder="Your email" style="padding: 10px 20px;">
+          <div class="btn btn-form" onclick="sendMsg()">Send</div><!--여기서 인증 이메일 보냄-->
+        </div>
+        <div id="timeLimit"class="text-start mt-1"></div>
+        <input type="password" class="form-control form-rounded mct-2 form-mobile" placeholder="Your Password" style="padding: 10px 20px;">
+        <button type="submit" class="btn-rounded text-white text-center mct-1" style="
+        font-size: 18px;width: 100%;background-color: #17375E;padding: 12px;">Find your PW</button>
+        <div class="separator mt-3" style="color: #fff;">&</div>
+        <p class="text-start" style="font-size: 18px;color: #fff">Your PW is...</p>
+        <div class="pwbox" style="color: #fff;">dfd</div><!--위에 버튼 누르면 여기서 비밀번호 생성-->
+        <div class="text-center" style="font-size:24px;color: #71a3ce;">Enjoy Your Service!</div>
+        <div class="d-flex mt-3 justify-content-center" style="font-size: 18px;">
+          <div style="color: #bfbfbf;">Do you have an Account?</div>
+          <div class="ms-2" style="color: #59dbe0;cursor: pointer;" onclick="changeContent('main')">Sign in</div>
+        </div>
+        
+      </div>
         `
     }
 }
